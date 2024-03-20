33b9d262aa63b2635d3f8087d7346c0b6bf14b96
AbhishekKargeti1 <abhishek.kargeti1@gmail.com>
2024-03-20 17:46:52 +0530

    TryCatch in Program

--- Project 1 of java Guessing Game/Project1.java
+++ Project 1 of java Guessing Game/Project1.java
@@ -1,11 +1,13 @@
 import java.util.*;
 class Project1{
     public static void main(String []args){
+        // String  s1 = "yes"; 
         Random r1= new Random();
         int Computergenratednumber = r1.nextInt(100);
         Scanner sc = new Scanner(System.in);
         for(;;){ 
-        System.out.println("Guess Number Between 1 to 100 ");
+        System.out.println("Guess Number Between 0 to 100 ");
+        try{
         int guessnumber = sc.nextInt();
         if(Computergenratednumber == guessnumber){
             System.out.println("You guessed right.\n You number is : "+guessnumber);
@@ -16,9 +18,16 @@ class Project1{
         }else{
             System.out.println("You gussed number is too Big");
             continue;
+                }
+            }catch(InputMismatchException i){
+                System.out.println("Please enter a valid number");
+                break;
+                // System.out.println("You want to continue ?");
+
+                
             }
         }
-        sc.close();
+
     }
-    
-}
+}    
+
