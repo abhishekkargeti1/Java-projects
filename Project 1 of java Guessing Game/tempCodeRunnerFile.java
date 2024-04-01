3a98348167e0b1ebd364a0afd4abfc9454e4c2c7
AbhishekKargeti1 <abhishek.kargeti1@gmail.com>
2024-04-01 21:14:30 +0530

    Guessing Game Gui version

---
+++ Project 1 of java Guessing Game/tempCodeRunnerFile.java
@@ -0,0 +1,5 @@
+f(s1 == 'Y'||s1 == 'y'){       
+                    if(Computergenratednumber == guessnumber){
+                        System.out.println("You guessed right.\n You number is : "+guessnumber);
+                        System.out.println("You Guessed the number in  "+guesscounter+" times");
+                        break;
